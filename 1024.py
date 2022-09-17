4ea84b3fedf78157f4e8aca4b8b2c69d69daa57b
zqqe <40433575+zqqe@users.noreply.github.com>
2022-09-17 09:06:24 +0800

    Update 1024.py

--- 1024.py
+++ 1024.py
@@ -5,7 +5,7 @@ import onetimepass as otp
 from time import sleep
 from urllib import parse
 import os
-from getver2 import GetVerificationCode
+from getver2 import apitruecaptcha
 from multiprocessing import Pool
 from config import config
 
